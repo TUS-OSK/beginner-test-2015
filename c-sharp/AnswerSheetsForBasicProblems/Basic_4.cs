4580eb16c796fd2a49110fb783bae24bc8cb1b1f
Kyoro_p <longcorridor@outlook.jp>
2015-08-08 10:17:40 +0900

    解答用紙作成
    basic問題4

---
+++ c-sharp/AnswerSheetsForBasicProblems/Basic_4.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_4
+{
+    class Program
+    {
+        //(1)
+        static void PrintOskConsciousness(int n)
+        {
+              //ここを埋める
+        }
+
+        //(2)
+        /*
+        *(i)返値の型
+        *
+        *(ii)引数の型
+        *
+        */
+
+        //(3)
+
+
+        //(4)
+
+
+        //(5)
+
+
+        //(6)
+
+
+        static void Main(string[] args)
+        {
+          //上で書いたコードを試したいときはここに書けばよい
+        }
+    }
+}
